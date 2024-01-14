f9511f82ab12c35f403e2c6b543633d883947c84
KostBoh <bohdan.kostya@gmail.com>
2024-01-14 15:38:19 +0200

    add import export

---
+++ src/js/helper.js
@@ -0,0 +1,18 @@
+// const c = 10;
+// const d = 15;
+// function add(a, b) {
+//   logger(a + b + c);
+//   return a + b + c;
+// }
+// class User {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+// }
+// function logger(value) {
+//   console.log(`Function logger work value is ${value}`);
+// }
+// export { add, User };
+// export default c;
+// export default d;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
-console.log('hell world');
+// console.log('hello world');
+// import { add, User } from './js/helper';
+// import * as module from './js/helper';
+// import { add as moduleAdd } from './js/helper';
+// import value from './js/helper';
+// function add(a, b) {
+//   return a + b + 50;
+// }
+// console.log(value);
+// console.log(moduleAdd(2, 2));
+// console.log(add(2, 2));
+
+// const c = 20;
+// console.log(add(2, 2));
+// console.log(new User('User', 20));
